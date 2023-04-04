b6373fc943d30f8f66d91cb62725eb13b1f7e973
Chris Loi <loichris97@gmail.com>
2023-04-04 04:02:13 -0700

    fixed layouts

--- app/src/main/java/com/example/mydailys/MainActivity.java
+++ app/src/main/java/com/example/mydailys/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.mydailys;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.lang.ref.Reference;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +29,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        taskViewModel = ViewModelProviders.of(this).get(TaskViewModel.class);
+        FloatingActionButton buttonAddTask = findViewById(R.id.button_add_task);
+
+        ActivityResultLauncher<Intent> addTaskLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        String title = data.getStringExtra(AddTaskActivity.EXTRA_TITLE);
+                        String description = data.getStringExtra(AddTaskActivity.EXTRA_DESCRIPTION);
+                        int priority = data.getIntExtra(AddTaskActivity.EXTRA_PRIORITY, 1);
+
+                        Task task = new Task(title, description, priority);
+                        taskViewModel.insert(task);
+
+                        Toast.makeText(this, "Task saved", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Task not saved", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                addTaskLauncher.launch(intent);
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        TaskAdapter adapter = new TaskAdapter();
+        recyclerView.setAdapter(adapter);
+
+        taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
         taskViewModel.getAllTasks().observe(this, new Observer<List<Task>>() {
             @Override
             public void onChanged(List<Task> tasks) {
-                // update RecyclerView later
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setTasks(tasks);
             }
         });
     }
