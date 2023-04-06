216aacb17a9598cc6cd153baa5f2562fb9482684
Chris Loi <loichris97@gmail.com>
2023-04-06 05:40:23 -0700

    calendarview

--- app/src/main/java/com/example/mydailys/BottomSheetFragment.java
+++ app/src/main/java/com/example/mydailys/BottomSheetFragment.java
@@ -1,17 +1,44 @@
 package com.example.mydailys;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.example.mydailys.model.Priority;
+import com.example.mydailys.model.Task;
+import com.example.mydailys.model.TaskViewModel;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.chip.Chip;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+    private EditText enterTodo;
+    private ImageButton calendarButton;
+    private ImageButton priorityButton;
+    private RadioGroup priorityRadioGroup;
+    private RadioButton selectedRadioButton;
+    private int selectedButtonId;
+    private ImageButton saveButton;
+    private CalendarView calendarView;
+    private Group calendarGroup;
+    private Date dueDate;
+    Calendar calendar = Calendar.getInstance();
 
     public BottomSheetFragment() {
     }
@@ -20,6 +47,17 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        calendarGroup = view.findViewById(R.id.calendar_group);
+        calendarView = view.findViewById(R.id.calendar_view);
+        calendarButton = view.findViewById(R.id.today_calendar_button);
+        enterTodo = view.findViewById(R.id.enter_todo_et);
+        saveButton = view.findViewById(R.id.save_todo_button);
+        priorityButton = view.findViewById(R.id.priority_todo_button);
+        priorityRadioGroup = view.findViewById(R.id.radioGroup_priority);
+
+        Chip todayChip = view.findViewById(R.id.today_chip);
+        Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
 
 
         return view;
@@ -27,6 +65,32 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        calendarButton.setOnClickListener(view12 -> {
+            calendarGroup.setVisibility(
+                    calendarGroup.getVisibility() == View.GONE ? View.VISIBLE : View.GONE
+            );
+
+        });
+        calendarView.setOnDateChangeListener((calendarView, year, month, dayOfMonth) -> {
+            calendar.clear();
+            calendar.set(year, month, dayOfMonth);
+            dueDate = calendar.getTime();
+
+            // Log.d("Cal", "onViewCreated: ===> month" + (month + 1) + ", dayOfMonth" + dayOfMonth);
+        });
+
+        saveButton.setOnClickListener(view1 -> {
+            String task = enterTodo.getText().toString().trim();
+            if (!TextUtils.isEmpty(task) && dueDate != null) {
+                Task myTask = new Task(task, Priority.HIGH,
+                        dueDate, Calendar.getInstance().getTime(),
+                        false);
+
+                TaskViewModel.insertTask(myTask);
+            }
+
+        });
 
     }
 }
